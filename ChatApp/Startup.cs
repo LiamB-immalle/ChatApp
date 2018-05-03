c586fb597a708ad18aae286300e375f17dd831d9
liamb-immalle <liam.brangers@immalle.be>
2018-05-03 20:54:26 +0200

    Edited small stuff n things

--- ChatApp/Startup.cs
+++ ChatApp/Startup.cs
@@ -64,7 +64,7 @@ namespace ChatApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Berichten}/{action=Index}/{id?}");
             });
         }
     }
